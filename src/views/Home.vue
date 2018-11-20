827fb8e2084ca004a002806e68574124cf5fdab7
Joe Gallotta <joeygallotta@gmail.com>
2018-11-20 12:58:31 -0600

    connect backend api and display some data on html page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+  <li v-for="dog in dogs">
+    {{ dog.meme_phrase }}
+  </li>
+</ul>
   </div>
 </template>
 
@@ -8,13 +12,21 @@
 </style>
 
 <script>
+var axios = require('axios');
+
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      message: "Welcome to Vue.js!",
+      dogs:[]
     };
   },
-  created: function() {},
+  created: function() {
+    axios.get('http://localhost:3000/api/dogs').then(function(response) {
+      console.log(response.data);
+      this.dogs = response.data;
+    }.bind(this));
+  },
   methods: {},
   computed: {}
 };
